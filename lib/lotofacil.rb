f1de401154e3ee83236a1489da130bd7c1792416
fabiotoledo79 <fabiotoledo79@yahoo.com.br>
2015-12-02 10:27:35 -0200

    identação

--- lib/lotofacil.rb
+++ lib/lotofacil.rb
@@ -1,9 +1,9 @@
 class Lotofacil
   def generate(qtde = 1)
-  	(0..(qtde.to_i - 1)).map{raffle_number}
+    (0..(qtde.to_i - 1)).map{raffle_number}
   end
 private
   def raffle_number
-  	Array(1..25).sample(15).sort
+    Array(1..25).sample(15).sort
   end
 end
